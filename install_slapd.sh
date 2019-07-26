07b6ac98886a7587130912fa2f45ceb6e318d30b
ericterkpertey <53063608+ericterkpertey@users.noreply.github.com>
2019-07-26 18:23:22 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -2,15 +2,15 @@
 
 sudo apt-get update
 
-export DEBIAN_FRONTEND='non-interactive'
+export DEBIAN_FRONTEND=noninteractive
 echo -e "slapd slapd/root_password password eric" |sudo debconf-set-selections
 echo -e "slapd slapd/root_password_again password eric" |sudo debconf-set-selections
 echo -e "slapd slapd/internal/adminpw password eric" |sudo debconf-set-selections
 echo -e "slapd slapd/internal/generated_adminpw password eric" |sudo debconf-set-selections
 echo -e "slapd slapd/password2 password eric" |sudo debconf-set-selections
 echo -e "slapd slapd/password1 password eric" |sudo debconf-set-selections
-echo -e "slapd slapd/domain string clemson.cloudlab" |sudo debconf-set-selections
-echo -e "slapd shared/organization string IT410" |sudo debconf-set-selections
+echo -e "slapd slapd/domain string clemson.cloudlab.us" |sudo debconf-set-selections
+echo -e "slapd shared/organization string WestChester" |sudo debconf-set-selections
 echo -e "slapd slapd/backend string MDB" |sudo debconf-set-selections
 echo -e "slapd slapd/purge_database boolean false" |sudo debconf-set-selections
 echo -e "slapd slapd/move_old_database boolean true" |sudo debconf-set-selections
