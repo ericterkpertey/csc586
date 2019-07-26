5ea81d1968b9820b085668d53f2f724459ad1292
ericterkpertey <53063608+ericterkpertey@users.noreply.github.com>
2019-07-26 18:07:01 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
 sudo apt-get update
-#sudo apt-get install -y slapd ldap-utils
 
 export DEBIAN_FRONTEND='non-interactive'
-echo -e "slapd slapd/root_password password eric" |debconf-set-selections
-echo -e "slapd slapd/root_password_again password eric" |debconf-set-selections
-echo -e "slapd slapd/internal/adminpw password eric" |debconf-set-selections
-echo -e "slapd slapd/internal/generated_adminpw password eric" |debconf-set-selections
-echo -e "slapd slapd/password2 password eric" |debconf-set-selections
-echo -e "slapd slapd/password1 password eric" |debconf-set-selections
-echo -e "slapd slapd/domain string clemson.cloudlab" |debconf-set-selections
-echo -e "slapd shared/organization string IT410" |debconf-set-selections
-echo -e "slapd slapd/backend string MDB" |debconf-set-selections
-echo -e "slapd slapd/purge_database boolean false" |debconf-set-selections
-echo -e "slapd slapd/move_old_database boolean true" |debconf-set-selections
-echo -e "slapd slapd/allow_ldap_v2 boolean false" |debconf-set-selections
-echo -e "slapd slapd/no_configuration boolean false" |debconf-set-selections
+echo -e "slapd slapd/root_password password eric" |sudo debconf-set-selections
+echo -e "slapd slapd/root_password_again password eric" |sudo debconf-set-selections
+echo -e "slapd slapd/internal/adminpw password eric" |sudo debconf-set-selections
+echo -e "slapd slapd/internal/generated_adminpw password eric" |sudo debconf-set-selections
+echo -e "slapd slapd/password2 password eric" |sudo debconf-set-selections
+echo -e "slapd slapd/password1 password eric" |sudo debconf-set-selections
+echo -e "slapd slapd/domain string clemson.cloudlab" |sudo debconf-set-selections
+echo -e "slapd shared/organization string IT410" |sudo debconf-set-selections
+echo -e "slapd slapd/backend string MDB" |sudo debconf-set-selections
+echo -e "slapd slapd/purge_database boolean false" |sudo debconf-set-selections
+echo -e "slapd slapd/move_old_database boolean true" |sudo debconf-set-selections
+echo -e "slapd slapd/allow_ldap_v2 boolean false" |sudo debconf-set-selections
+echo -e "slapd slapd/no_configuration boolean false" |sudo debconf-set-selections
 
 # Grab slapd and ldap-utils (pre-seeded)
-#apt-get install -y slapd ldap-utils phpldapadmin
-
-# Must reconfigure slapd for it to work properly 
-#sudo dpkg-reconfigure slapd 
+sudo apt-get install -y slapd ldap-utils
+ 
 #sudo ufw allow ldap
 
