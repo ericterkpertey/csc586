f58cd8c73f8b20283db2e5d3ca8a7fa9aaba8747
ericterkpertey <53063608+ericterkpertey@users.noreply.github.com>
2019-07-30 13:46:29 -0400

    Update client_ldap.sh

--- client_ldap.sh
+++ client_ldap.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+ldap-auth-config        ldap-auth-config/bindpw password
+ldap-auth-config        ldap-auth-config/rootbindpw     password
+ldap-auth-config        ldap-auth-config/pam_password   select  md5
+ldap-auth-config        ldap-auth-config/move-to-debconf        boolean true
+ldap-auth-config        ldap-auth-config/binddn string  cn=proxyuser,dc=example,dc=net
+ldap-auth-config        ldap-auth-config/dblogin        boolean false
+ldap-auth-config        ldap-auth-config/override       boolean true
+libpam-runtime  libpam-runtime/profiles multiselect     unix, ldap, systemd, capability
+ldap-auth-config        ldap-auth-config/ldapns/ldap_version    select  3
+ldap-auth-config        ldap-auth-config/dbrootlogin    boolean true
+ldap-auth-config        ldap-auth-config/ldapns/ldap-server     string  ldap://192.168.1.1
+ldap-auth-config        ldap-auth-config/ldapns/base-dn string  dc=clemson,dc=cloudlab, dc=us
+ldap-auth-config        ldap-auth-config/rootbinddn     string  cn=admin,dc=clemson,dc=cloudlab, dc=us
